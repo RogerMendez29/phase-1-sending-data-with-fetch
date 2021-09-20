85743896d4e6fafc7e973d020fad05bf54bccf45
Roger Mendez <roger61087@gmail.com>
2021-09-20 15:10:02 -0400

    commit tree dd974a9bc9c882c42004b3d1cdf0d476a25322d2

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+// let data = {
+//   name: "Steve",
+//   email: "steve@steve.com",
+// };
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((resp) => resp.json())
+    .then((data) => renderId(data))
+    .catch((error) => {
+      renderErrMsg(error);
+    });
+}
+
+const renderId = (data) => {
+  document.body.innerHTML = data.id;
+};
+
+function renderErrMsg(error) {
+  document.body.innerHTML = error.message;
+}
